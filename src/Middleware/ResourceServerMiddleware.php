b74851a66c39b4bc6498d43da7b31e420b3ddfd8
hooman naghiee <hooman@RTLer.com>
2016-05-25 23:41:02 +0430

    add ResourceServerMiddleware

---
+++ src/Middleware/ResourceServerMiddleware.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace RTLer\Oauth2\Middleware;
+
+use Closure;
+use League\OAuth2\Server\Exception\OAuthServerException;
+
+class ResourceServerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $psrRequest = app()->make('Psr\Http\Message\ServerRequestInterface');
+        $resourceServer = \Oauth2::makeResourceServer();
+
+        $serverRequest = $resourceServer->validateAuthenticatedRequest($psrRequest);
+        $environment = \Oauth2::getOptions()['environment'];
+
+        $neededScopes = array_slice(func_get_args(), 2);
+        $requestedScopes = $serverRequest->getAttribute('oauth_scopes');
+        $this->validateScopes($neededScopes, $requestedScopes);
+
+        $userVerifier = \Oauth2::getOptions()['user_verifier'];
+        $user = (new $userVerifier())
+            ->getUserByIdentifier($serverRequest->getAttribute('oauth_user_id'));
+        if ($environment == 'laravel') {
+            \Auth::setUser($user);
+        }
+        $request->setUserResolver(function () use ($serverRequest) {
+            return ;
+        });
+        return $next($request);
+    }
+
+    /**
+     * @param $neededScopes
+     * @param $requestedScopes
+     * @throws OAuthServerException
+     */
+    protected function validateScopes($neededScopes, $requestedScopes)
+    {
+        $haveRightScope = true;
+        foreach ($neededScopes as $neededScope) {
+            if (!in_array($neededScope, $requestedScopes)) {
+                $haveRightScope = false;
+            }
+        }
+        if (!$haveRightScope) {
+            throw OAuthServerException::accessDenied(
+                'you need right scope to access this resource'
+            );
+        }
+    }
+}
